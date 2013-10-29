0dd1eff4c206a34b236a6e872bb88d46a7032286
RoyDong <roy.dongwei@gmail.com>
2013-10-29 23:34:08 +0800

    tb

--- controller/index.go
+++ controller/index.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+    "fmt"
     "github.com/roydong/potato"
 )
 
@@ -9,8 +10,21 @@ type Index struct {
 }
 
 func (c *Index) Show() {
-    d := map[string]string{"name": "Roy", "message": "hello"}
+    d := map[string]string{
+        "name": "Roy",
+        "time": fmt.Sprintf("%d", c.Request.Session.LastActivity),
+    }
+
+    c.Request.Session.Mount("user", "Roy Dong", true)
     c.Render("index", d)
+}
+
+func (c *Index) Home() {
+    user,_ := c.Request.Session.String("user");
+    d := map[string]string{
+        "user": user,
+    }
+    c.Render("home", d)
 }
 
 
