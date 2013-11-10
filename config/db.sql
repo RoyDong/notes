4b03730dee860422be345db532971a19703e7479
RoyDong <roy.dongwei@gmail.com>
2013-11-10 17:30:56 +0800

    tb

--- config/db.sql
+++ config/db.sql
@@ -18,7 +18,9 @@ CREATE TABLE `topic` (
       `id` bigint(11) NOT NULL AUTO_INCREMENT,
       `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
       `content` text CHARACTER SET utf8,
+      `state` int(11) NOT NULL DEFAULT '0',
       `created_at` bigint(20) unsigned NOT NULL DEFAULT '0',
       `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0',
-      PRIMARY KEY (`id`)
+      PRIMARY KEY (`id`),
+      KEY `title` (`title`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
